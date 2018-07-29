e12f04fc8861bfd175969c9d6f44a0c33b8fb101
Alexander Richards <electrodeyt@gmail.com>
2018-07-29 17:36:52 +0200

    First Commit from git

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 make: little-init.cpp
-	g++ -o init little-init.cpp -std=c++11
+	g++ -o init little-init.cpp -std=c++11 -static
 clean:
 	-rm init 2> /dev/null; true
 
 install:
-	-@echo To install the init run "make install-init", this will delete /sbin/init!
+	-@echo To install the init run "make install-init", the existing init will be renamed to init.bak
 
 install-init:
-	-sudo rm /sbin/init
+	-sudo cp /sbin/init /sbin/init.bak
 	sudo cp init /sbin/init
--- little-init.cpp
+++ little-init.cpp
@@ -1,5 +1,9 @@
 /* Little Init by Alexander Richards
    under MIT; Please compile staticly */
+
+///TODO:
+/// runlevel-switching
+
 //Include stuff
 
 #include <signal.h>
@@ -12,77 +16,97 @@
 #include <sys/reboot.h>
 #include <sstream>
 #include <thread>
+#include <chrono>
 #include <fstream>
 #include <string.h>
 
 #define MAX_BUF 1024
+
 // #include <time.h>
 using namespace std;
+
 // Declare Vars
-float version = 0.2;
-int start_exit_code;
-int done = 0;
-int shell_exit;
-int fd;
-char * fifo_pipe = "/tmp/init_fifo";
-char buf[MAX_BUF];
-int runlevel;
-const char * rlevel_char;
+int done = 0; // is init procedure complete?
+int runlevel = 1; // runlevel, 0 should always be shutdown
+string version = "simple-init v0.3-git";
+string rc_script = "/etc/init.d/rc";
+// not the final solution, but its the best thing for now, there are more important things :)
 
-// Define Functions
-void print(string msg) {
-cout << msg << endl;
+// https://www.systutorials.com/5510/catching-the-signal-sent-by-kill-in-c-on-linux/ please don't sue me
+void sighandler(int signum, siginfo_t *info, void *ptr) { // so i can turn it off gracefully for now
+    	if (signum == SIGTERM) {
+      		sync();
+      		reboot(RB_POWER_OFF);
+    	} else if (signum == SIGHUP) {
+      		sync();
+      		reboot(RB_AUTOBOOT);
+    	}
 }
 
-void noend_print(string msg) {
-cout << msg;
-}
-
-// https://www.systutorials.com/5510/catching-the-signal-sent-by-kill-in-c-on-linux/ Please don't sue me
-void sighandler(int signum, siginfo_t *info, void *ptr)
-{
-    if (signum == SIGTERM) {
-      sync();
-      reboot(RB_POWER_OFF);
-    } else if (signum == SIGHUP) {
-      sync();
-      reboot(RB_AUTOBOOT);
-    }
-}
-
-void catch_sig()
-{
-    static struct sigaction _sigact;
-    memset(&_sigact, 0, sizeof(_sigact));
-    _sigact.sa_sigaction = sighandler;
-    _sigact.sa_flags = SA_SIGINFO;
-    sigaction(SIGTERM, &_sigact, NULL);
+void catch_sig() {
+ 	static struct sigaction _sigact;
+    	memset(&_sigact, 0, sizeof(_sigact));
+   	_sigact.sa_sigaction = sighandler;
+   	_sigact.sa_flags = SA_SIGINFO;
+   	sigaction(SIGTERM, &_sigact, NULL);
+   	sigaction(SIGHUP, &_sigact, NULL);
 }
 //END
-inline bool exist(const std::string& name) {
-ifstream file(name);
-if (file.good()) {return true;} else {return false;}} // Simple file checker; This line is probably not required but why not
-void init() {
-print("Remounting Root as Read-Write!");
-std::system("mount -o remount,rw /");
-print("Entering Runlevel 3...");
-print("Bringing up processes!");
-start_exit_code = std::system("sh /etc/init.d/rc 3");
-noend_print("rc script run, exit code "); cout << start_exit_code / 256 << endl;
-done = 1;
-runlevel = 3;
-if (exist("/linit/c_start.sh")) {
-print("Custom start script exists; Also starting it");
-std::system("/linit/c_start.sh");
+
+void call_rc(int rl) {
+  if(rl == 9) {
+    system(string(rc_script + string(" S")).c_str());
+  } else {
+    system(string(rc_script + string(" ") + to_string(rl)).c_str());
+  }
 }
 
-while(true){}
-catch_sig();
+inline bool exist(const std::string& name) {
+	ifstream file(name);
+	if (file.good()) {return true;} else {return false;}
+} // Simple file checker; This line is probably not required but why not
+
+void init() {
+  system("mount -a"); // just mount everything now
+	system("mount -o remount,rw /"); // remount root as rw, just in case its ro
+
+	done = 1;
+	runlevel = 3; // still have to implement switching it
+	if(exist("/etc/init.d/shell")) {
+		pid_t pid = fork();
+		if(pid == 0) { // if child process, run the "shell" symlink, hopefully :)
+			execl("/etc/init.d/shell", "/etc/init.d/shell", (char*) 0);
+			this_thread::sleep_for(std::chrono::milliseconds(1000));
+      kill(getpid(), SIGKILL);
+		}
+	}
 }
+
 
 // Call said functions
 int main() {
-noend_print("Little Init v"); cout << version; print(" Booting!");
-print("");
-init();
+	cout << version << endl;
+  if(getpid() == 1) {
+    init();
+    while(true) { // do stuff
+      if(exist("/tmp/runlevel")) {
+        int tmp_runlevel = 0; // runlevel from /tmp/runlevel. default = 0
+        string temp;
+        ifstream file("/tmp/runlevel");
+        if(file.is_open()) {
+          getline(file, temp);
+          // temp should have the runlevel in it. 9 is alias for S
+          if(temp == "") {
+            // nothing, continue
+          } else {
+            tmp_runlevel = stoi(temp);
+            call_rc(tmp_runlevel);
+          }
+        }
+        file.close();
+      }
+    }
+  } else {
+    // should be reserved for runlevel changes
+  }
 }
